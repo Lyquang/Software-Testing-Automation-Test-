977214fc09b8be6dad3d6cb00f4b7d808992dcd6
Cong Phuong <bonanhphuong@gmail.com>
2025-12-07 19:00:22 +0700

    Phuong: done refactor editAccount and customerLogin

--- scripts/customerLogin_lv2.py
+++ scripts/customerLogin_lv2.py
@@ -2,7 +2,6 @@ import csv
 import unittest
 import time
 import os
-from dotenv import load_dotenv
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -13,23 +12,16 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-env_path = os.path.join(current_dir, 'customerLogin.env')
 
-load_dotenv(dotenv_path=env_path)
-
-if os.getenv("BASE_URL") is None:
-    raise Exception(f"LỖI: Không tìm thấy file '{env_path}' hoặc file rỗng!")
-
-def load_csv_data(filename):
+def load_csv_data():
     rows = []
-    csv_path = os.path.join(current_dir, filename)
     try:
-        with open(csv_path, encoding="utf-8") as f:
+        with open("../data/customerLogin_lv2.csv", encoding="utf-8") as f:
             reader = csv.DictReader(f)
             for row in reader:
                 rows.append(row)
     except FileNotFoundError:
-        print(f"LỖI: Không tìm thấy file CSV tại: {csv_path}")
+        print(f"LỖI: Không tìm thấy file CSV tại: ../data/customerLogin_lv2.csv")
     return rows
 
 class CustomerLoginTest(unittest.TestCase):
@@ -45,42 +37,46 @@ class CustomerLoginTest(unittest.TestCase):
         self.driver.maximize_window()
         self.clear_cache()
         
-        self.base_url = os.getenv("BASE_URL")
 
     def tearDown(self):
         self.driver.quit()
 
-    def login_flow(self, user_name, expected_text):
+    def login_flow(self, row_data):
         driver = self.driver
         wait = WebDriverWait(driver, 10)
 
-        driver.get(self.base_url)
+        base_url = row_data['BASE_URL']
+        driver.get(base_url)
         
-        cus_login_xpath = os.getenv("LOCATOR_BTN_CUSTOMER_LOGIN_XPATH")
+        cus_login_xpath = row_data['LOCATOR_BTN_CUSTOMER_LOGIN_XPATH']
         cus_login_btn = wait.until(EC.element_to_be_clickable((By.XPATH, cus_login_xpath)))
         cus_login_btn.click()
 
-        user_select_id = os.getenv("LOCATOR_SELECT_USER_ID")
+        user_select_id = row_data['LOCATOR_SELECT_USER_ID']
         dropdown_element = wait.until(EC.visibility_of_element_located((By.ID, user_select_id)))
         select_obj = Select(dropdown_element)
 
         all_options_text = [option.text for option in select_obj.options]
 
+        user_name = row_data['userName']
+
         if user_name in all_options_text:
             select_obj.select_by_visible_text(user_name)
             
-            login_btn_xpath = os.getenv("LOCATOR_BTN_LOGIN_XPATH")
+            login_btn_xpath = row_data['LOCATOR_BTN_LOGIN_XPATH']
             login_btn = wait.until(EC.element_to_be_clickable((By.XPATH, login_btn_xpath)))
             login_btn.click()
             
             time.sleep(1)
             
-            welcome_msg_class = os.getenv("LOCATOR_MSG_WELCOME_CLASS")
+            welcome_msg_class = row_data['LOCATOR_MSG_WELCOME_CLASS']
             welcome_msg = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, welcome_msg_class))).text
+            
+            expected_text = row_data['expectedWelcomeText']
             self.assertIn(expected_text, welcome_msg)
 
         else:
-            login_btn_xpath = os.getenv("LOCATOR_BTN_LOGIN_XPATH")
+            login_btn_xpath = row_data['LOCATOR_BTN_LOGIN_XPATH']
             login_buttons = driver.find_elements(By.XPATH, login_btn_xpath)
 
             is_login_visible = False
@@ -91,20 +87,16 @@ class CustomerLoginTest(unittest.TestCase):
             self.assertFalse(is_login_visible, "Lỗi: Nút Login vẫn hiện ra dù chưa chọn User hợp lệ!")
 
 def generate_test_cases():
-    print("Chạy testcase customerLogin_lv2.py")
-    csv_filename = os.getenv("CSV_FILE")
     
-    if not csv_filename:
+    data_rows = load_csv_data() 
+    
+    if not data_rows:
+        print("Không có dữ liệu để chạy test.")
         return
-
-    data_rows = load_csv_data(csv_filename) 
 
     for index, row in enumerate(data_rows):
         def test(self, row=row): 
-            self.login_flow(
-                row["userName"],
-                row["expectedWelcomeText"]
-            )
+            self.login_flow(row)
         
         safe_name = row['userName'].replace(" ", "_")
         test_name = f"test_login_{index+1}_{safe_name}"
--- scripts/editAccount_lv2.py
+++ scripts/editAccount_lv2.py
@@ -2,7 +2,6 @@ import csv
 import unittest
 import time
 import os
-from dotenv import load_dotenv
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -12,23 +11,16 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-env_path = os.path.join(current_dir, 'editAccount.env')
 
-load_dotenv(dotenv_path=env_path)
-
-if os.getenv("BASE_URL") is None:
-    raise Exception(f"LỖI: Không tìm thấy file '{env_path}' hoặc file rỗng!")
-
-def load_csv_data(filename):
+def load_csv_data():
     rows = []
-    csv_path = os.path.join(current_dir, filename)
     try:
-        with open(csv_path, encoding="utf-8") as f:
+        with open("../data/editAccount_lv2.csv", encoding="utf-8") as f:
             reader = csv.DictReader(f)
             for row in reader:
                 rows.append(row)
     except FileNotFoundError:
-        print(f"Không tìm thấy file {csv_path}")
+        print(f"LỖI: Không tìm thấy file ../data/editAccount_lv2.csv")
     return rows
 
 class EditAccountTest(unittest.TestCase):
@@ -49,12 +41,10 @@ class EditAccountTest(unittest.TestCase):
         }
         
         options.add_experimental_option("prefs", prefs)
-        
         options.add_argument("--disable-notifications")
         options.add_argument("--disable-infobars")
         options.add_argument("--disable-popup-blocking")
         options.add_argument("--disable-features=PasswordLeakDetection,SafeBrowsing")
-        
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option('useAutomationExtension', False)
         
@@ -62,67 +52,71 @@ class EditAccountTest(unittest.TestCase):
         self.driver.implicitly_wait(10)
         self.driver.maximize_window()
         
-        self.base_url = os.getenv("BASE_URL")
 
     def tearDown(self):
         self.driver.quit()
 
-    def login(self):
+    def login(self, row_data):
         driver = self.driver
         wait = WebDriverWait(driver, 10)
         
-        driver.get(self.base_url)
+        base_url = row_data['BASE_URL']
+        driver.get(base_url)
         
-        email_id = os.getenv("LOCATOR_INPUT_EMAIL_ID")
-        test_email = os.getenv("TEST_EMAIL")
+        email_id = row_data['LOCATOR_INPUT_EMAIL_ID']
+        test_email = row_data['TEST_EMAIL']
+        
         email_input = wait.until(EC.visibility_of_element_located((By.ID, email_id)))
         email_input.clear()
         email_input.send_keys(test_email)
         
-        pass_id = os.getenv("LOCATOR_INPUT_PASSWORD_ID")
-        test_pass = os.getenv("TEST_PASSWORD")
+        pass_id = row_data['LOCATOR_INPUT_PASSWORD_ID']
+        test_pass = row_data['TEST_PASSWORD']
+        
         pass_input = driver.find_element(By.ID, pass_id)
         pass_input.clear()
         pass_input.send_keys(test_pass)
         
-        login_xpath = os.getenv("LOCATOR_BTN_LOGIN_XPATH")
+        login_xpath = row_data['LOCATOR_BTN_LOGIN_XPATH']
         login_btn = driver.find_element(By.XPATH, login_xpath)
         login_btn.click()
 
-    def edit_account_flow(self, first_name, telephone, expected_type, expected_msg):
+    def edit_account_flow(self, row_data):
         driver = self.driver
         wait = WebDriverWait(driver, 30) 
 
-        self.login()
+        self.login(row_data)
 
-        edit_link_text = os.getenv("LOCATOR_LINK_EDIT_TEXT")
+        edit_link_text = row_data['LOCATOR_LINK_EDIT_TEXT']
         edit_link = wait.until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, edit_link_text)))
         edit_link.click()
 
-        fname_id = os.getenv("LOCATOR_INPUT_FIRSTNAME_ID")
+        fname_id = row_data['LOCATOR_INPUT_FIRSTNAME_ID']
         fname_input = wait.until(EC.visibility_of_element_located((By.ID, fname_id)))
         
-        phone_id = os.getenv("LOCATOR_INPUT_TELEPHONE_ID")
+        phone_id = row_data['LOCATOR_INPUT_TELEPHONE_ID']
         phone_input = driver.find_element(By.ID, phone_id)
 
         fname_input.clear()
-        fname_input.send_keys(first_name)
+        fname_input.send_keys(row_data['firstName'])
         
         phone_input.clear()
-        phone_input.send_keys(telephone)
+        phone_input.send_keys(row_data['telephone'])
 
-        continue_xpath = os.getenv("LOCATOR_BTN_CONTINUE_XPATH")
+        continue_xpath = row_data['LOCATOR_BTN_CONTINUE_XPATH']
         continue_btn = driver.find_element(By.XPATH, continue_xpath)
         continue_btn.click()
 
+        expected_type = row_data['expectedType']
+        expected_msg = row_data['expectedMessage']
+
         if expected_type == "success":
-            
             try:
                 wait.until(EC.url_contains("route=account/account"))
             except:
                 self.fail("Form submit quá lâu hoặc không thành công, chưa chuyển hướng về trang Account.")
 
-            success_class = os.getenv("LOCATOR_MSG_SUCCESS_CLASS")
+            success_class = row_data['LOCATOR_MSG_SUCCESS_CLASS']
             success_alert = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, success_class)))
             self.assertIn(expected_msg, success_alert.text)
 
@@ -132,26 +126,19 @@ class EditAccountTest(unittest.TestCase):
                 error_element = wait.until(EC.visibility_of_element_located((By.XPATH, error_xpath)))
                 self.assertTrue(error_element.is_displayed())
             except:
-                driver.save_screenshot(f"error_fail_{first_name}.png")
                 self.fail(f"Fail: Không tìm thấy thông báo lỗi mong đợi: '{expected_msg}'")
 
 def generate_test_cases():
-    print("Chạy testcase editAccount_lv2.py")
-    csv_filename = os.getenv("CSV_FILE")
     
-    if not csv_filename:
+    data_rows = load_csv_data() 
+    
+    if not data_rows:
+        print("Không có dữ liệu để chạy test.")
         return
-
-    data_rows = load_csv_data(csv_filename) 
 
     for index, row in enumerate(data_rows):
         def test(self, row=row): 
-            self.edit_account_flow(
-                row["firstName"],
-                row["telephone"],
-                row["expectedType"],
-                row["expectedMessage"]
-            )
+            self.edit_account_flow(row)
         
         safe_name = "Valid" if row['expectedType'] == 'success' else "Invalid"
         test_name = f"test_edit_{index+1}_{safe_name}"
